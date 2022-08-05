461d4a3752d4688ec111affddca649622f0d78a0
Himanshi Kapoor <kapoorhimanshi617@gmail.com>
2022-08-05 22:14:38 +0530

    Added day-13 files

---
+++ Day-13/exercise-2.py
@@ -0,0 +1,14 @@
+# Missing type conversion from type str to int
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
